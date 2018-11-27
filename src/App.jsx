2894fed56cc24c46d48992edc250734db8f15464
Chris <chrisstanarsenault@gmail.com>
2018-11-27 11:24:07 -0500

    set initial loading state of app

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import ChatBar from './ChatBar.jsx'
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {loading: true};
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false});
+    }, 3000)
+  }
+
   render() {
+    if (this.state.loading) {
+      return <h1>Loading messages....</h1>
+    } else {
     return (
       <div>
         <MessageList />
         <ChatBar />
       </div>
     );
+    }
   }
 }
 export default App;
