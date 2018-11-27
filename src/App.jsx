961ec94c215e203329e23fa94c6094422e114982
Chris <chrisstanarsenault@gmail.com>
2018-11-27 17:17:39 -0500

    added functionality to add new messages on Enter keypress

--- src/App.jsx
+++ src/App.jsx
@@ -21,24 +21,66 @@ class App extends Component {
         }
       ]
     };
+
+    this.addNewMessageItem = this.addNewMessageItem.bind(this);
   }
 
+
   componentDidMount() {
+    console.log("componentDidMount <App />");
     setTimeout(() => {
-      this.setState({loading: false});
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {
+        id: 3,
+        username: "Michelle",
+        content: "Hello there!"
+      };
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      //  Calling setState will trigger a call to render() in App and all child components
+      this.setState({
+        loading: false,
+        messages: messages
+      });
     }, 3000)
-   }
+  }
+
+  // generateRandomId() {
+  //   const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   const alphabetLength = alphabet.length;
+  //   const randoIter = (key, n) => {
+  //     if (n === 0) {
+  //       return key;
+  //     }
+  //     const randoIndex = Math.floor(Math.random() * alphabetLength);
+  //     const randoLetter = alphabet[randoIndex];
+  //     return randoIter(key + randoLetter, n - 1);
+  //   };
+  //   return randoIter("", 10);
+  // };
+
+  addNewMessageItem(content) {
+    const newMessage = {
+      id: Math.floor(Math.random() * 100),
+      username: this.state.currentUser.name,
+      content
+    };
+    const oldMessageItems = this.state.messages;
+    const newMessageItems = [...oldMessageItems, newMessage]
+    this.setState({messages: newMessageItems});
+  }
 
   render() {
     if (this.state.loading) {
       return <h1>Loading messages....</h1>
-    } else {
-    return (
+      } else {
+      return (
       <div>
         <MessageList messages={this.state.messages} />
-        <ChatBar user={this.state.currentUser.name} />
+        <ChatBar user={this.state.currentUser.name} addNewMessageItem={this.addNewMessageItem} />
       </div>
-    );
+      );
     }
   }
 }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 
 class ChatBar extends Component {
+  handleKeyPress = e => {
+    if (e.key === "Enter") {
+      const contentInput = e.target.value;
+      this.props.addNewMessageItem(contentInput);
+      e.target.value = "";
+    }
+  };
+
   render() {
+    // const onSubmit = evt => {
+    //   evt.preventDefault();
+    //   const newMessageInput = evt.target.elements.messageContent;
+    //   //console.log(newMessageInput.value)
+    //   const userInput = evt.target.elements.user;
+    //   //console.log(userInput.value)
+    //   addnewMessageItem(userInput.value, newMessageInput.value);
+
+    //   newMessageInput.value = "";
+    // }
+
     return (
-
-        <footer className="chatbar">
-          <input className="chatbar-username" defaultValue={this.props.user} />
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-        </footer>
-
+      <footer className="chatbar">
+        <input
+          className="chatbar-username"
+          name="user"
+          defaultValue={this.props.user}
+        />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          name="messageContent"
+          onKeyPress={this.handleKeyPress}
+        />
+      </footer>
     );
   }
 }
