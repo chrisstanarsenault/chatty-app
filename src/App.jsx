83ee94fc291520429c9323dd9b2f7da78f34798c
Chris <chrisstanarsenault@gmail.com>
2018-11-28 14:24:04 -0500

    set up websockets server and client side to talk to eachother so users can see messages on both sides

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,45 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx'
 import MessageList from './MessageList.jsx';
 
+
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      loading: true,
+      //loading: true,
       currentUser: { name: "Bob" }, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
-        {
-          id: "4f46b23",
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: "h73b9f8",
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+        // {
+        //   id: "4f46b23",
+        //   username: "Bob",
+        //   content: "Has anyone seen my marbles?",
+        // },
+        // {
+        //   id: "h73b9f8",
+        //   username: "Anonymous",
+        //   content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        // }
+      ] // meesages coming from the server will be stored here as they arrive
     };
+    this.socket = new WebSocket("ws://localhost:3001");
 
     this.addNewMessageItem = this.addNewMessageItem.bind(this);
   }
 
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {
-        id: 3,
-        username: "Michelle",
-        content: "Hello there!"
-      };
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      //  Calling setState will trigger a call to render() in App and all child components
-      this.setState({
-        loading: false,
-        messages: messages
-      });
-    }, 3000)
+    this.socket.onopen = function (e) {
+      console.log('Connected to: ' + e.currentTarget.url);
+    };
+    this.socket.onmessage = (newMessage) => {
+      console.log(newMessage.data)
+      const oldMessageItems = this.state.messages;
+      const newMessageItems = JSON.parse(event.data)
+      const actualMessage = [...oldMessageItems, newMessageItems]
+      this.setState({messages: actualMessage})
+
+    }
   }
 
   // generateRandomId() {
@@ -62,19 +59,20 @@ class App extends Component {
 
   addNewMessageItem(content) {
     const newMessage = {
-      id: Math.floor(Math.random() * 100),
+      //id: Math.floor(Math.random() * 100),
       username: this.state.currentUser.name,
       content
     };
-    const oldMessageItems = this.state.messages;
-    const newMessageItems = [...oldMessageItems, newMessage]
-    this.setState({messages: newMessageItems});
+    //  const oldMessageItems = this.state.messages;
+    //  const newMessageItems = [...oldMessageItems, newMessage]
+    //  this.setState({messages: newMessageItems});
+    this.socket.send(JSON.stringify(newMessage));
   }
 
   render() {
-    if (this.state.loading) {
-      return <h1>Loading messages....</h1>
-      } else {
+    // if (this.state.loading) {
+    //   return <h1>Loading messages....</h1>
+    //   } else {
       return (
       <div>
         <MessageList messages={this.state.messages} />
@@ -83,5 +81,5 @@ class App extends Component {
       );
     }
   }
-}
+//}
 export default App;
