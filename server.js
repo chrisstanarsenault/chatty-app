564289ec80cef85cdb20d012b51fe40b8ac5540a
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-12-17 22:14:29 -0500

    another h test

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var config = require("./webpack.config");
+
+let PORT = process.env.PORT || 3000;
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000,
-      ignored: /node_modules/
-    }
-  })
-  .listen(process.env.PORT || 3000, '0.0.0.0', function (err, result) {
-    if (err) {
-      console.log(err);
-    }
+  publicPath: config.output.publicPath,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+    ignored: /node_modules/
+  }
+}).listen(PORT, "0.0.0.0", function(err, result) {
+  if (err) {
+    console.log(err);
+  }
 
-    console.log('Running at http://0.0.0.0:3000');
-  });
+  console.log(`Running at ${PORT}`);
+});
